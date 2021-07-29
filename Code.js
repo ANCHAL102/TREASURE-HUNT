e61a907b1b758b154509a933962e41f0746c4c69
ANCHAL102 <80636070+ANCHAL102@users.noreply.github.com>
2021-07-29 21:59:49 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,24 @@
+class Code{
+constructor(){
+}
+clues() {
+
+    fill("white")
+    textSize(15)
+    text("D L N I A D A ", 100,50)
+    fill("lightblue")
+    text("Hint : OWNER OF THE JINIE!", 100,70)
+
+    fill("white")  
+    textSize(15)
+    text(" Y S E R U C I T", 700,150) 
+    fill("lightblue")
+    text("Hint: USE TO PROTECT! ", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("T I M E ", 100,320)
+    fill("lightblue")
+    text("Hint: WAITS FOR NONE!", 100,340)
+}
+}
